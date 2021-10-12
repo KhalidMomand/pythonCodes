fc71c687600a8595f6eaf4d6ab601c5318e5fcc8
khalid <mohammadkhalidmomand@gmail.com>
2021-10-13 01:29:52 +0400

    This script will takes two list and return its new version which is concatniated index wise

---
+++ index_wise_concat.py
@@ -0,0 +1,32 @@
+#!/usrs/bin/env python3
+
+'''
+Task: 
+write a program to concatinate two different lists index - wise, but the list must have the size of items
+Example: aList = ["m" , "na", "i", "Kha"] bList = ["y", "me", "s", "lid"] result will be cList = ["my", "name", "is", "khalid"]
+'''
+
+
+#let's define a function for this task
+
+
+def index_wise_concat(a_list, b_list):
+   c_list = []
+   for item1, item2 in zip(aList,bList):
+      new_item = item1 + item2
+      c_list.append(new_item)
+
+   return c_list
+
+
+# Now let's create two lists
+aList = ['m', 'na', 'i', 'kha']
+bList = ['y', 'me', 's', 'lid']
+
+# let's excute the program and check if it is working
+
+if __name__ == "__main__":
+   print(index_wise_concat(aList,bList))
+
+
+
